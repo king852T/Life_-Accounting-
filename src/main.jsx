fa830034a7031b180a9948e6877c33b02a219cd3
king852T <103150077+king852T@users.noreply.github.com>
2025-12-19 14:30:54 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import LifeAccountingApp' from './App.jsx'
+import LifeAccountingApp from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <LifeAccountingApp/>
+  <React.StrictMode>
+    <LifeAccountingApp />
   </React.StrictMode>,
 )
