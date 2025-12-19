7861e4b3397d4d80ee4ba788b480c33218a66c12
king852T <103150077+king852T@users.noreply.github.com>
2025-12-19 15:54:29 -0500

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import LifeAccountingApp from './App.jsx'
+import LifeAccountingApp from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
